bdcf683ceec5efdcab6ef8325260ec65a47d3b65
NinKorn <NinKorn@163.com>
2019-07-22 10:55:58 +0800

    安装依赖

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './style/app.scss'
 function App() {
   return (
     <div className="App">
-      
+      <div className="header">
+          <span>首页</span>
+          <span>TodoList</span>
+      </div>
     </div>
   );
 }
